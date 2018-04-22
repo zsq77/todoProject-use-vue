b443d582ff98b0da461107be1b79e969ccb5be45
zhengshiqi <597720334@qq.com>
2018-04-23 01:02:00 +0800

    项目基本结构搭建完成

---
+++ src/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="test">{{text}}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: "史琪加油"
+    };
+  }
+};
+</script>
+
+<style>
+#test {
+  color: red;
+}
+</style>
+
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path')
+const HTMLPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+const isDev = process.env.NODE_ENV === 'development'
+
+
+const config = {
+  target: 'web',
+  entry: path.join(__dirname, 'src/index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.styl$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'stylus-loader'
+        ]
+      },
+      {
+        test: /\.(gif|jpg|jpeg|png|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1024,
+              name: '[name]-aaa.[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: isDev ? '"development"' : '"production"'
+      }
+    }),
+    new HTMLPlugin()
+  ]
+}
+if (isDev) {
+  config.devServer = {
+    port: 9000,
+    host: '0.0.0.0',
+    overlay: {
+      errors: true
+    }
+  }
+}
+module.exports = config
